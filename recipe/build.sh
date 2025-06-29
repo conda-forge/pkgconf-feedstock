0c524d70bc7d2e8b347bd109054dfe487fbc5969
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-29 14:43:54 -0700

    fix CFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,11 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export CFLAGS="${CFLAGS} -D_POSIX_C_SOURCE=200809L"
+if [[ ${target_platform} =~ .*linux.* ]]; then
+    export CFLAGS="${CFLAGS} -D_POSIX_C_SOURCE=200809L"
+else
+    export CFLAGS="${CFLAGS} -D_DARWIN_C_SOURCE"
+fi
 
 meson ${MESON_ARGS} --wrap-mode=nofallback build
 meson compile -C build -v
